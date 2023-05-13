ce5462253d864e5472e1adf7a9f9f357a81ffaf0
Ivan Kuzmanović <ivan.kuzmanovic@comtrade.com>
2023-05-13 20:49:50 +0200

    kreiran logout test. u basepage klasi ciscenje, u homepage klasi je kreirane su metode koje su potrebne za test

---
+++ Tests/LogoutTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace AutomationFramework.Tests
+{
+    public class LogoutTest : BaseTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // User login
+            Pages.LoginPage.Login(
+                TestData.TestData.Login.username, 
+                TestData.TestData.Login.password
+            );
+        }
+
+        [Test]
+        public void LogoutUser()
+        {
+            Pages.HomePage.ClickOnLogoutLink();
+
+            // Asertacija testa - Proverava da li postoji link logout
+            Assert.IsFalse(Pages.HomePage.IsLinkDisplayed("logout2"));
+        }
+    }
+}
